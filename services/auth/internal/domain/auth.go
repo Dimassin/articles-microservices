40fc59ccd885a282a12c32806fafd605c7981704
dima_maximov_git <dimassin121@mail.ru>
2026-03-22 22:29:36 +0300

    git init

---
+++ services/auth/internal/domain/auth.go
@@ -0,0 +1,20 @@
+package domain
+
+type RegisterRequest struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+	Username string `json:"username"`
+}
+
+type LoginRequest struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+type AuthResponse struct {
+	AccessToken  string `json:"access_token"`
+	RefreshToken string `json:"refresh_token"`
+	UserID       string `json:"user_id"`
+	Email        string `json:"email"`
+	Username     string `json:"username"`
+}
---
+++ services/auth/internal/ports/user_repository.go
@@ -0,0 +1,14 @@
+package ports
+
+import (
+	"Articles/services/auth/internal/domain"
+	"context"
+)
+
+type UserRepository interface {
+	Create(ctx context.Context, user *domain.User) error
+
+	FindByEmail(ctx context.Context, email string) (*domain.User, error)
+
+	FindByID(ctx context.Context, id string) (*domain.User, error)
+}
