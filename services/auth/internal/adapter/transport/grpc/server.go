fa7d192100253ed75b9fc572d109b3da1242abc6
dima_maximov_git <dimassin121@mail.ru>
2026-03-26 22:26:21 +0300

    proto fix

--- services/auth/internal/adapter/transport/grpc/server.go
+++ services/auth/internal/adapter/transport/grpc/server.go
@@ -1,1 +1,35 @@
 package grpc
+
+import (
+	"auth/internal/usecase"
+	"context"
+
+	pb "github.com/Dimassin/articles-microservices/proto/auth"
+)
+
+type AuthGrpcServer struct {
+	pb.UnimplementedAuthServiceServer
+	authUsecase *usecase.AuthUsecase
+}
+
+func NewAuthGrpcServer(authUsecase *usecase.AuthUsecase) *AuthGrpcServer {
+	return &AuthGrpcServer{
+		authUsecase: authUsecase,
+	}
+}
+
+func (s *AuthGrpcServer) ValidateToken(ctx context.Context, req *pb.ValidateTokenRequest) (*pb.ValidateTokenResponse, error) {
+	claims, err := s.authUsecase.ValidateToken(ctx, req.Token)
+	if err != nil {
+		return &pb.ValidateTokenResponse{
+			Valid: false,
+		}, nil
+	}
+
+	return &pb.ValidateTokenResponse{
+		UserId:   claims.UserID,
+		Email:    claims.Email,
+		Username: claims.Username,
+		Valid:    true,
+	}, nil
+}
